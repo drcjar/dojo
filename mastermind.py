bc314860f1af1a72b1fb0613495eaf8742c94934
Thomas Hunger <tehunger@gmail.com>
2012-01-05 21:11:17 +0000

    done

--- mastermind.py
+++ mastermind.py
@@ -22,7 +22,29 @@ def test_guess(correct, guess):
 			ib += 1
 
 	return black, white
-	
+
+
+def autoguess(correct):
+	options = [(a, b, c, d) for a in range(6) for b in range(6) for c in range(6) for d in range(6)]
+
+	steps = 0
+
+	while(len(options) > 1):
+          steps += 1		
+	  guess = options[1]
+	  options2 = options[:]
+	  guess_result = test_guess(correct, guess)
+
+	  for i in range(len(options2)):
+	    if guess_result[0] < test_guess(options2[i],guess)[0]:
+	      options.remove(options2[i])
+	      continue
+
+	    if guess_result[1] < test_guess(options2[i],guess)[1]:
+	      options.remove(options2[i])
+
+	print 'Solution: {}, steps: {}'.format(options[0], steps)
+
 if __name__ == "__main__":
 	six_diseases = ['plague', 'gout', 'scurvy', 'cancer', 'gangrene', 'syphillis']
 	correct = [random.choice(range(6)) for i in xrange(4)]
@@ -30,6 +52,11 @@ if __name__ == "__main__":
 
 	enum_diseases = enumerate(six_diseases)
 	print ''.join('{} {}\n'.format(i, d) for i, d in enum_diseases)	
+	
+	autoguess(correct)
+	print '-' * 80
+	print "Now you"
+	print '-' * 80
 
 	for x in xrange(10):
 		print 'Guess {} out of 10. What is your guess?'.format(x+1)
